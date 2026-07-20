00704ab8218a83137f976e0c4d33a5139d48e12d
Kevin Huang <huangyf9883@gmail.com>
2026-07-21 04:56:15 +0800

    chore(deps): Update dependencies

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,6 @@
-import { defineCollection, z } from 'astro:content'
+import { defineCollection } from 'astro:content'
 import { glob } from 'astro/loaders'
+import { z } from 'zod'
 
 // 定义内容集合 schema
 const pages = defineCollection({
